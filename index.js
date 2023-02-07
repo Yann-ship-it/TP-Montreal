d98fdd89a26b63847b0d3a731ebcea1524ae9a58
Yann-ship-it <yy.iris85@gmail.com>
2023-02-07 16:32:47 +0100

    JS trie

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // Menu burger
 const menu = document.getElementById("menu");
-const burger = document.getElementById("burger");
 const ul = document.querySelector("ul");
 
 menu.addEventListener("click", function (e) {
   e.preventDefault();
-  burger.classList.toggle("open");
   menu.classList.toggle("open");
   ul.classList.toggle("open");
 });
