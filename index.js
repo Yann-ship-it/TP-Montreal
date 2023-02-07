de607ecc588ae4078d3a8f40fc7cc2ad6f04e43e
Yann-ship-it <yy.iris85@gmail.com>
2023-02-07 16:31:50 +0100

    Prettier

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // ScrollTop
-document.addEventListener('DOMContentLoaded', function() {
-    window.onscroll = function(ev) {
-      document.getElementById("scrollTop").className = (window.pageYOffset > 100) ? "visible" : "none";
-    };
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  window.onscroll = function (ev) {
+    document.getElementById("scrollTop").className =
+      window.pageYOffset > 100 ? "visible" : "none";
+  };
+});
 
 // Menu burger
-const menu = document.getElementById('menu')
-const burger = document.getElementById('burger')
-const ul = document.querySelector('ul')
+const menu = document.getElementById("menu");
+const burger = document.getElementById("burger");
+const ul = document.querySelector("ul");
 
-menu.addEventListener('click', function(e) {
-  e.preventDefault()
-  burger.classList.toggle('open')
-  menu.classList.toggle('open')
-  ul.classList.toggle('open')
-})
+menu.addEventListener("click", function (e) {
+  e.preventDefault();
+  burger.classList.toggle("open");
+  menu.classList.toggle("open");
+  ul.classList.toggle("open");
+});
